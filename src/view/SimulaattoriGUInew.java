cf39631639ec3ff77a346a6285f8686f3a12331e
johanlav <91597079+johanlav@users.noreply.github.com>
2022-03-06 11:10:25 +0200

    Layout edited and theme added

--- src/view/SimulaattoriGUInew.java
+++ src/view/SimulaattoriGUInew.java
@@ -4,7 +4,10 @@ import javafx.scene.canvas.Canvas;
 import java.io.IOException;
 
 import controller.IKontrolleriVtoM;
+import controller.KasinoKontrolleri;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,13 +16,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import simu.framework.Trace;
+import simu.framework.Trace.Level;
 
 public class SimulaattoriGUInew extends Application implements ISimulaattorinUI{
 	
 	private Stage stage;
 	private BorderPane root;
 	
-	@FXML private Canvas canvas;
 	@FXML private Button aloitusBtn;
 	@FXML private Button pysäytysBtn;
 	@FXML private Button uusikäynistysBtn;
@@ -35,18 +39,34 @@ public class SimulaattoriGUInew extends Application implements ISimulaattorinUI{
 	@FXML private Label avgPaihtymysID;
 	@FXML private Label avgVarallisuusID;
 	@FXML private Label avgLapimenoID;
+	@FXML private BorderPane canvas;
+	@FXML private AnchorPane alapaneelit;
 	
 	
-	private IKontrolleriVtoM kontrolleri;
+	private IKontrolleriVtoM kontrolleri = null;
 	private IVisualisointi view = null;
+	private static KasinoVisualisointi visualisointi;
+	
+	@Override
+	public void init(){
+		
+		Trace.setTraceLevel(Level.INFO);
+		
+		kontrolleri = new KasinoKontrolleri(this);
+		
+	}
 	
 	@Override
 	public void start(Stage stage){
 		try {
 			this.stage = stage;
 			this.stage.setTitle("Kasino Simulaattori");
+			init();
+			
 			rootLayout();
+			
 			setAlapaneelit();
+			naytaTulokset();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -72,11 +92,9 @@ public class SimulaattoriGUInew extends Application implements ISimulaattorinUI{
 	public void setAlapaneelit() {
 		try {
 			FXMLLoader loader= new FXMLLoader();
-			loader.setLocation(getClass().getResource("Alapaneelit.fxml"));
-			AnchorPane alapaneelit = (AnchorPane) loader.load();
-			
+			loader.setLocation(getClass().getResource("Layout.fxml"));
+			alapaneelit = (AnchorPane) loader.load();
 			root.setCenter(alapaneelit);
-			
 			
 		}
 		catch(IOException e) {
@@ -84,12 +102,29 @@ public class SimulaattoriGUInew extends Application implements ISimulaattorinUI{
 		}
 	}
 	
-	public void handelStart() {
-		if (view == null){
-			view = new VisualisointiNew(canvas);
-			view.tyhjennaNaytto();
-		}
-		aloitusBtn.setDisable(true);
+	public void setCanvas() throws IOException {
+		FXMLLoader loader= new FXMLLoader();
+		loader.setLocation(getClass().getResource("Layout.fxml"));
+		canvas = (BorderPane) loader.load();
+		canvas.setCenter(visualisointi.getKanvas());
+	}
+	
+	public void naytaTulokset() {
+		double tulokset[] = kontrolleri.haeTulokset();
+		System.out.println(tulokset[0]);
+		aikaID.setText(Double.toString(tulokset[0]));
+	}
+	
+	public void handleStart() {
+		aloitusBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	kontrolleri.kaynnistaSimulointi();
+                naytaTulokset();
+                aloitusBtn.setDisable(true);
+            }
+            });
+
 	}
 	
 	public static void main(String[] args) {
