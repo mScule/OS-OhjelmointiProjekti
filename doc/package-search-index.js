b8eff034f16c312a50e89abd578bc4b5584a87b1
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-03-12 21:15:20 +0200

    Generate a javadoc with everything except eduni.distributions

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"kasinoSimulaattori.controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"kasinoSimulaattori"},{"l":"kasinoSimulaattori.controller"},{"l":"kasinoSimulaattori.simu.framework"},{"l":"kasinoSimulaattori.simu.model"},{"l":"kasinoSimulaattori.util"},{"l":"kasinoSimulaattori.view"},{"l":"kasinoSimulaattori.view.animated"},{"l":"model"}];updateSearchResults();
