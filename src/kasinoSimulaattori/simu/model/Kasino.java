9181311c7939089a71c5f25d0512c53b131f0f0b
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-03-13 13:28:17 +0200

    Add Javadoc comments to Kasino.java and clean code

--- src/kasinoSimulaattori/simu/model/Kasino.java
+++ src/kasinoSimulaattori/simu/model/Kasino.java
@@ -5,34 +5,73 @@ import kasinoSimulaattori.eduni.distributions.Normal;
 import kasinoSimulaattori.eduni.distributions.Uniform;
 import kasinoSimulaattori.simu.framework.Kello;
 
+/**
+ * Luokka, joka säilöö ja määrittelee kasinon erilaisia parametrejä.
+ * 
+ * @author Jonathan Methuen
+ */
 public final class Kasino {
+    /**
+     * Kasinon lähtörahamäärä, jonka arvo on 1000000
+     */
     public final static int kasinonLahtoRahat = 1000000;
-    // Mitä rahamäärää asiakkaan varakkuus kun "double = 1" merkitsee:
+    /**
+     * Kertoo sen, mitä rahamäärää se tarkoittaa, kun asiakkaan varakkuus double = 1
+     */
     public final static int asiakkaanVarakkuus1Double = 50000;
-    private static double kasinonRahat = kasinonLahtoRahat;
-    private static double asiakkaidenKeskimMieliala = 0;
-    private static double asiakkaidenKeskimPaihtyneisyys = 0;
-    private static double asiakkaidenKeskimVarakkuus = 0;
-    private static double yllapitohinta = 0;
+    /**
+     * Kasinon palveluaikojen keskimääräinen oletus palveluaika
+     */
+    public final static double defaultKeskimPalveluaika = 10;
+
+    /**
+     * Kasinolle saapuvien asiakkaiden keskimääräinen oletusväliaika
+     */
+    public final static double defaultKeskimSaapumisaika = 10;
+    /**
+     * Yhden blackjack pöydän lisääminen kasinolle kasvattaa kasinon kuluja sadalla.
+     */
+    public final static double pelipoydanHinta = 100;
+    /**
+     * Yhden baarin lisääminen kasinolle kasvattaa kasinon kuluja kolmellasadalla.
+     */
+    public final static double baarinHinta = 300;
+    /**
+     * Yhden sisäänkäynnin lisääminen kasinolle kasvattaa kasinon kuluja tuhannella.
+     */
+    public final static double sisaankaynninHinta = 1000;
+    /**
+     * Yhden uloskaynnin lisääminen kasinolle kasvattaa kasinon kuluja tuhannella.
+     */
+    public final static double uloskaynninHinta = 1000;
+    /**
+     * Tämän muutujan arvo heikentää kasinoon käytettävän rahamäärän hyötyjä. Isompi
+     * arvo heikentää hyötyjä enemmän.
+     */
+    public final static double investmentInefficiencyRatio = 200;
+    /**
+     * Kasinon minimiylläpitohinta on 100
+     */
     private final static double minimiYllapitohinta = 100;
-    private static double mainoskulut = 0;
+
     private static long seed = 1337;
+    /**
+     * Sisäänkäynnin, uloskäynnin ja baarin palveluaikojen oletusjakauma
+     */
+    public static Negexp defaultPalveluajatNegexp = new Negexp(defaultKeskimPalveluaika, seed);
+    /**
+     * Kasinolle saapuvien asiakkaiden saapumisväliaikojen oletusjakauma
+     */
+    public static Negexp defaultSaapumisajatNegexp = new Negexp(defaultKeskimSaapumisaika, seed);
+
     // Asiakkaiden ominaisuuksien jakauma.
     private static Normal asiakasOminNormal = new Normal(0, 0.5, seed);
-    // Pelien jakauma.
+    // Pelien jakauma
     private static Uniform pelitUniform = new Uniform(0, 1, seed);
-    // Sisäänkäynnin, uloskäynnin ja baarin palveluaikajakauma.
-    public final static double defaultKeskimPalveluaika = 10;
-    public static Negexp defaultPalveluajatNegexp = new Negexp(defaultKeskimPalveluaika, seed);
-    public final static double defaultKeskimSaapumisaika = 10;
-    public static Negexp defaultSaapumisajatNegexp = new Negexp(defaultKeskimSaapumisaika, seed);
-    public final static double pelipoydanHinta = 100;
-    public final static double baarinHinta = 300;
-    public final static double sisaankaynninHinta = 1000;
-    public final static double uloskaynninHinta = 1000;
-    private static boolean vararikko = false;
-    public final static double investmentInefficiencyRatio = 200;
 
+    private static double kasinonRahat = kasinonLahtoRahat;
+    private static double yllapitohinta = minimiYllapitohinta;
+    private static double mainoskulut = 0;
     private static double keskimPalveluaika = 10;
     private static double keskimSaapumisvaliaika = 10;
     private static double minBet = 100;
@@ -41,194 +80,325 @@ public final class Kasino {
     private static double blackjackTasapeliprosentti = 0.0848;
 
     private static boolean pause;
+    private static boolean vararikko = false;
 
     private Kasino() {
-
     }
 
+    /**
+     * Hakee onko kasinosimulaatio taukotilassa vai ei.
+     * 
+     * @return False, jos simulaatio ei ole taukotilassa ja true jos on
+     */
     public static boolean isPause() {
         return pause;
     }
 
+    /**
+     * Asettaa simulaation taukotilaan tai lopettaa simulaation taukotilan.
+     * 
+     * @param pause True asettaa simulaation taukotilaan ja false lopettaa
+     *              simulaation taukotilan.
+     */
     public static void setPause(boolean pause) {
         Kasino.pause = pause;
     }
 
+    /**
+     * Palauttaa kasinon takaisin lähtöarvoihin
+     */
     public static void resetKasino() {
-        // Resetoi tämä class:
+
+        // Palauta tämä luokka oletusarvoihinsa
 
         kasinonRahat = kasinonLahtoRahat;
         asiakkaidenKeskimMieliala = 0;
         asiakkaidenKeskimPaihtyneisyys = 0;
         asiakkaidenKeskimVarakkuus = 0;
-        yllapitohinta = 0;
+        yllapitohinta = minimiYllapitohinta;
         mainoskulut = 0;
         seed = 1337;
 
         defaultSaapumisajatNegexp = new Negexp(defaultKeskimSaapumisaika, seed);
-        // Asiakkaiden ominaisuuksien jakauma.
         asiakasOminNormal = new Normal(0, 0.5, seed);
-        // Pelien jakauma.
         pelitUniform = new Uniform(0, 1, seed);
-        // Sisäänkäynnin, uloskäynnin ja baarin palveluaikajakauma.
         defaultPalveluajatNegexp = new Negexp(defaultKeskimPalveluaika, seed);
 
         vararikko = false;
 
-        keskimPalveluaika = 10;
+        keskimPalveluaika = defaultKeskimPalveluaika;
         keskimSaapumisvaliaika = 10;
         minBet = 100;
         maxBet = 1000;
         blackjackVoittoprosentti = 0.4222;
         blackjackTasapeliprosentti = 0.0848;
 
-        // Resetoi asiakkaat
+        // Palauta asiakkaat oletusarvoihinsa
 
         Asiakas.i = 1;
 
         Palvelupiste.palveluid = 0;
-        
-        // Resetoi kellonaika
-        
+
+        // Nolla kellonaika
+
         Kello.getInstance().setAika(0);
     }
 
+    /**
+     * Hakee kasinon blackjack pelin asiakkaiden voittoprosentin
+     * 
+     * @return Kasinon blackjack pelin asiakkaiden voittoprosentti
+     */
     public static double getBlackjackVoittoprosentti() {
         return blackjackVoittoprosentti;
     }
 
+    /**
+     * Asettaa kasinon blackjack pelin asiakkaiden voittoprosentin
+     * 
+     * @param blackjackVoittoprosentti Asetettava kasinon blackjack pelin
+     *                                 asiakkaiden voittoprosentti
+     */
     public static void setBlackjackVoittoprosentti(double blackjackVoittoprosentti) {
         Kasino.blackjackVoittoprosentti = blackjackVoittoprosentti;
     }
 
+    /**
+     * Hakee kasinon blackjack pelin tasapeliprosentin
+     * 
+     * @return Kasinon blackjack pelin tasapeliprosentin
+     */
     public static double getBlackjackTasapeliprosentti() {
         return blackjackTasapeliprosentti;
     }
 
+    /**
+     * Asettaa kasinon blackjack pelin tasapeliprosentin
+     * 
+     * @param blackjackVoittoprosentti Asetettava kasinon blackjack pelin
+     *                                 tasapeliprosentin
+     */
     public static void setBlackjackTasapeliprosentti(double blackjackTasapeliprosentti) {
         Kasino.blackjackTasapeliprosentti = blackjackTasapeliprosentti;
     }
 
+    /**
+     * Hakee kasinon blackjack pelin minimipanoksen
+     * 
+     * @return Kasinon blackjack pelin minimipanos
+     */
     public static double getMinBet() {
         return minBet;
     }
 
+    /**
+     * Asettaa kasinon blackjack pelin minimipanoksen
+     * 
+     * @param minBet Asetettava kasinon blackjack pelin minimipanos
+     */
     public static void setMinBet(double minBet) {
         Kasino.minBet = minBet;
     }
 
+    /**
+     * Hakee kasinon blackjack pelin maksimipanoksen
+     * 
+     * @return Kasinon blackjack pelin maksimipanos
+     */
     public static double getMaxBet() {
         return maxBet;
     }
 
+    /**
+     * Asettaa kasinon blackjack pelin maksimipanoksen
+     * 
+     * @param minBet Asetettava kasinon blackjack pelin maksimipanos
+     */
     public static void setMaxBet(double maxBet) {
         Kasino.maxBet = maxBet;
     }
 
+    /**
+     * Hakee kasinon palveluaikojen keskimääräisen pituuden
+     * 
+     * @return Kasinon palveluaikojen keskimääräinen pituus
+     */
     public static double getKeskimPalveluaika() {
         return keskimPalveluaika;
     }
 
+    /**
+     * Asettaa kasinon palveluaikojen keskimääräisen pituuden
+     * 
+     * @param keskimPalveluaika Asetettava palveluaikojen keskimääräinen pituus
+     */
     public static void setKeskimPalveluaika(double keskimPalveluaika) {
         Kasino.keskimPalveluaika = keskimPalveluaika;
     }
 
+    /**
+     * Hakee asiakkaiden kasinoon saapumisväliaikojen keskimääräisen pituuden
+     * 
+     * @return Asiakkaiden kasinoon saapumisväliaikojen keskimääräinen pituus
+     */
     public static double getKeskimSaapumisvaliaika() {
         return keskimSaapumisvaliaika;
     }
 
+    /**
+     * Asettaa asiakkaiden kasinoon saapumisväliaikojen keskimääräisen pituuden
+     * 
+     * @param keskimSaapumisvaliaika Asetettava asiakkaiden kasinoon
+     *                               saapumisväliaikojen keskimääräinen pituus
+     */
     public static void setKeskimSaapumisvaliaika(double keskimSaapumisvaliaika) {
         Kasino.keskimSaapumisvaliaika = keskimSaapumisvaliaika;
     }
 
+    /**
+     * Hakee kasinon ylläpitohinnan
+     * 
+     * @return Kasinon ylläpitohinta
+     */
     public static double getYllapitohinta() {
         return yllapitohinta;
     }
 
-    public static long getSeed() {
-        return seed;
-    }
-
-    public static void setSeed(long seed) {
-        Kasino.seed = seed;
-    }
-
-    public static double getMainoskulut() {
-        return mainoskulut;
-    }
-
-    public static void setMainoskulut(double mainoskulut) {
-        Kasino.mainoskulut = mainoskulut;
-    }
-
-    public static double getMinimiyllapitohinta() {
-        return minimiYllapitohinta;
-    }
-
-    public static double getKokoYllapitohinta() {
-        return yllapitohinta + mainoskulut + minimiYllapitohinta;
-    }
-
+    /**
+     * Asettaa kasinon ylläpitohinnan
+     * 
+     * @param yllapitohinta Asetettava kasinon ylläpitohinta
+     */
     public static void setYllapitohinta(double yllapitohinta) {
         Kasino.yllapitohinta = yllapitohinta;
     }
 
+    /**
+     * Hakee kasinon jakaumien seedin
+     * 
+     * @return Kasinon jakaumien seed
+     */
+    public static long getSeed() {
+        return seed;
+    }
+
+    /**
+     * Asettaa kasinon jakaumien seedin
+     * 
+     * @param seed Asetettava kasinon jakaumien seed
+     */
+    public static void setSeed(long seed) {
+        Kasino.seed = seed;
+    }
+
+    /**
+     * Hakee kasinon mainoskulut
+     * 
+     * @return Kasinon mainoskulut
+     */
+    public static double getMainoskulut() {
+        return mainoskulut;
+    }
+
+    /**
+     * Asettaa kasinon mainoskulut
+     * 
+     * @param mainoskulut Asetettava kasinon mainoskulut
+     */
+    public static void setMainoskulut(double mainoskulut) {
+        Kasino.mainoskulut = mainoskulut;
+    }
+
+    /**
+     * Hakee kasinon minimi ylläpitohinnan
+     * 
+     * @return Kasinon minimi ylläpitohinta
+     */
+    public static double getMinimiyllapitohinta() {
+        return minimiYllapitohinta;
+    }
+
+    /**
+     * Hakee kasinon ylläpidon kulut kokonaisuudessaan
+     * 
+     * @return Kasinon ylläpidon kulut kokonaisuudessaan
+     */
+    public static double getKokoYllapitohinta() {
+        return yllapitohinta + mainoskulut + minimiYllapitohinta;
+    }
+
+    /**
+     * Hakee onko kasino vararikossa
+     * 
+     * @return True, jos kasino on vararikossa ja false jos ei ole
+     */
     public static boolean isVararikko() {
         return vararikko;
     }
 
+    /**
+     * Asettaa kasinon vararikkoon tai poistaa kasinon vararikosta
+     * 
+     * @param vararikko Asetetaan true, jos kasino laitetaan vararikkoon ja false
+     *                  jos poistetaan kasino vararikosta
+     */
     public static void setVararikko(boolean vararikko) {
         Kasino.vararikko = vararikko;
     }
 
+    /**
+     * Hakee kasinon asiakkaiden ominaisuuksien lähtöarvojen määrittelemiseen
+     * käytettävän normaalijakauman
+     * 
+     * @return Asiakkaiden ominaisuuksien lähtöarvojen määrittelemiseen
+     *         käytettävä normaalijakauma
+     */
     public static Normal getAsiakasOminNormal() {
         return asiakasOminNormal;
     }
 
+    /**
+     * Hakee kasinon pelien tulosten määrittelemisessä käytettävän tasaisen jakauman
+     * 
+     * @return Kasinon pelien tulosten määrittelemisessä käytettävä tasainen jakauma
+     */
     public static Uniform getPelitUniform() {
         return pelitUniform;
     }
 
+    /**
+     * Hakee kasinon tämänhetkisen rahamäärän
+     * 
+     * @return Kasinon tämänhetkinen rahamäärä
+     */
     public static double getKasinonRahat() {
         return kasinonRahat;
     }
 
+    /**
+     * Vähentää kasinolta parametrin arvon verran rahaa
+     * 
+     * @param rahamaara Kasinon rahoista vähennettävän rahamäärän suuruus
+     */
     public static void loseMoney(double rahamaara) {
         kasinonRahat -= rahamaara;
     }
 
+    /**
+     * Lisää kasinolle parametrin arvon verran rahaa
+     * 
+     * @param rahamaara Kasinon rahoihin lisättävän rahamäärän suuruus
+     */
     public static void gainMoney(double rahamaara) {
         kasinonRahat += rahamaara;
     }
 
+    /**
+     * Laskee ja palauttaa kasinon voittojen rahamäärän
+     * 
+     * @return Kasinon voittojen rahamäärä
+     */
     public static double getKasinonVoitto() {
         return kasinonRahat - kasinonLahtoRahat;
-    }
-
-    public static double getAsiakkaidenKeskimMieliala() {
-        return asiakkaidenKeskimMieliala;
-    }
-
-    public static void setAsiakkaidenKeskimMieliala(double asiakkaidenKeskimMieliala) {
-        // TODO päivitä asiakkaiden keskim. mielialaa joka kerta, kun jonkin asiakkaan
-        // mieliala muuttuu.
-    }
-
-    public static double getAsiakkaidenKeskimPaihtyneisyys() {
-        return asiakkaidenKeskimPaihtyneisyys;
-    }
-
-    public static void setAsiakkaidenKeskimPaihtyneisyys(double asiakkaidenKeskimPaihtyneisyys) {
-        // TODO päivitä asiakkaiden keskim. päihtyneisyys joka kerta, kun jonkin
-        // asiakkaan päihtyneisyys muuttuu.
-    }
-
-    public static double getAsiakkaidenKeskimVarakkuus() {
-        return asiakkaidenKeskimVarakkuus;
-    }
-
-    public static void setAsiakkaidenKeskimVarakkuus(double asiakkaidenKeskimVarakkuus) {
-        // TODO päivitä asiakkaiden keskim. varakkuus joka kerta, kun jonkin
-        // asiakkaan varakkuus muuttuu.
     }
 }
